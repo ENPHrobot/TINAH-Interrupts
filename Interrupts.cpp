d9e31c98586fb8934ab925ece799b28b236eee98
Andy Tu <andytu@alumni.ubc.ca>
2015-07-07 12:48:36 -0700

    add timer interrupt function

--- Interrupts.cpp
+++ Interrupts.cpp
@@ -7,11 +7,13 @@ void (*isr0)();
 void (*isr1)();
 void (*isr2)();
 void (*isr3)();
+void (*timer)();
 }
-ISR(INT0_vect) { (*isr0)();}
-ISR(INT1_vect) { (*isr1)();}
-ISR(INT2_vect) { (*isr2)();}
-ISR(INT3_vect) { (*isr3)();}
+ISR(INT0_vect) { (*isr0)(); }
+ISR(INT1_vect) { (*isr1)(); }
+ISR(INT2_vect) { (*isr2)(); }
+ISR(INT3_vect) { (*isr3)(); }
+ISR(TIMER3_COMPA_vect) { (*timer)(); }
 
 /*  Enables an external interrupt pin
 INTX: Which interrupt should be configured?
@@ -68,4 +70,38 @@ void attachISR(unsigned int INTX, void (*f)())
 		isr3 = f;
 		break;
 	}
+}
+
+/* Configures Timer1 to call an interrupt routine with the desired frequency  */
+/* The interrupt routine that is called is ISR(TIMER1_COMPA_vect)             */
+/* Valid interrupt frequencies: 1Hz to 65535 Hz                               */
+/* If the frequency is impossible to achieve, no interrupt will be configured */
+void attachTimerInterrupt(unsigned int interruptFrequencyHz, void (*f)())
+{
+    const uint32_t timerOverflowHz[] = {F_CPU / 1, F_CPU / 8, F_CPU / 64, F_CPU / 256, F_CPU / 1024};
+    for (uint8_t i = 0; i < 5; i++)
+    {
+        /* The number of 16-bit timer overflows needed to obtain the desired frequency */
+        const uint32_t overflowsNeeded = timerOverflowHz[i] / interruptFrequencyHz;
+        /* Check if the number of overflows can be stored in a 16-bit register */
+        if (overflowsNeeded <= 0xFFFFU)
+        {
+            cli();
+            TCCR3A = 0;                         /* Clear current comparison value */
+            TCNT3  = 0;                         /* Clear current timer value      */
+            OCR3A  = (uint16_t)overflowsNeeded; /* Set timer comparison value     */
+            TCCR3B = (1 << WGM12);              /* Set timer comparison mode      */
+            TCCR3B |= i + 1;                    /* Set timer prescaler value      */
+            ETIMSK |= (1 << OCIE3A);            /* Set timer interrupt enable     */
+            sei();
+            timer = f;
+            return;
+        }
+    }
+}
+
+/* Disables the Timer3A comparison interrupt */
+void detachTimerInterrupt()
+{
+    ETIMSK &= ~(1 << OCIE3A);
 }
